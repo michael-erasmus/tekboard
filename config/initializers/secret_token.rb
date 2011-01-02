40d1e4e76af86804357e7ace8df9c257326e438d
Michael Erasmus <michaelerasmus@gmail.com>
2011-01-02 11:38:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tekboard::Application.config.secret_token = 'cb24aeb89baa68342afd63f0abc2383b5d702b9bfc968bb0888c0ba45670a6cfcb7f5bb58e74b5b64a3bc9e1405578c3aadf9c043064a109be667b8dc5ca35fb'
