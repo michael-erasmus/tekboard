99139a2ecac5612e8531c004421360331819da96
Michael Erasmus <michaelerasmus@gmail.com>
2011-04-18 23:45:56 +0200

    Added the activated date to jobs and filtering
    I added a timestamp field on Jobs: activated_date. It will automatically
    be updated each time the job is saved with an active state
    
    This will then be used to know how long a job has been active.
    I will now have to write a task to deactivate jobs after 30 days
    I have also changed the filter on the home page to only show active jobs

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.3'
+gem 'rails'
 
 gem 'haml'
 gem 'barista', '~> 1.0'
--- app/models/job.rb
+++ app/models/job.rb
@@ -8,11 +8,18 @@ class Job < ActiveRecord::Base
   validates :company_link, :presence => true
   validates :company_email, :presence => true
 
-  def self.latest_approved
-    self.where("created_at > ? AND approved = ?", 5.days.ago, true)
-  end  
+  before_save :set_activation_date
+
+  def self.active
+    self.where(:state => 'active', :activated_date=> (Time.now.midnight - 30.days)..Time.now)
+  end
 
   def has_logo?()
     logo.file?
+  end
+
+  private
+  def set_activation_date
+    self.activated_date = Time.now if state == 'active'
   end
 end
