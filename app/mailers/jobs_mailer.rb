2c54abf39a7dd339d0bb4c839534c33a1508c8c8
Michael Erasmus <michaelerasmus@gmail.com>
2011-04-26 17:20:44 +0200

    Confirmation mail when job is activated
    Automating what I have been doing manually up until now.
    I added a action to the Jobs mailer to mail a confirmation mail to the
    employer when I have activated a job. The mail is sent in the after_save
    filter in the Job model if the status is active.

--- app/mailers/jobs_mailer.rb
+++ app/mailers/jobs_mailer.rb
@@ -6,4 +6,10 @@ class JobsMailer < ActionMailer::Base
     mail(:to => 'inbox@techboard.co.za',
          :subject => 'Techboard - A new job posting was added')
   end
+
+  def active_job(job)
+    @job = job
+    mail(:to => @job.company_email,
+         :subject => 'techboard.co.za - Your job post has been approved')
+  end
 end
