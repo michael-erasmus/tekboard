7513655434b25e36c1ce11c727d5d63d02036326
Michael Erasmus <michaelerasmus@gmail.com>
2011-04-26 21:41:56 +0200

    Back link on job detail

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,4 +2,8 @@ module JobsHelper
   def friendly_date_ago(job)
     time_ago_in_words(job.created_at)
   end
+
+  def back_home
+    link_to "Click here to go back to the home page", :action => "index" 
+  end
 end
